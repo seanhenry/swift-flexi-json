be373ac58600f90daf9f55314361cefe9aa16b4b
Sean Henry <hello@seanhenry.codes>
2016-05-19 00:24:42 +0100

    Adds initialisers for JSON strings and NSData

---
+++ FlexiJSONTests/FlexiJSON_ObjCTests.swift
@@ -0,0 +1,73 @@
+//
+//  FlexiJSON_ObjCTests.swift
+//
+//  Copyright © 2016 Sean Henry. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import XCTest
+@testable import FlexiJSON
+
+class FlexiJSON_ObjCTests: XCTestCase {
+    
+    // MARK: - init data
+
+    func test_init_data_shouldSetError_whenInvalidData() {
+        let json = FlexiJSON(data: NSData())
+        XCTAssertEqual(json.error, "Initialised FlexiJSON with invalid data.")
+    }
+
+    func test_init_data_shouldSetDictionary() {
+        let data = "{ \"simple\": \"json\" }".dataUsingEncoding(NSUTF8StringEncoding)!
+        let json = FlexiJSON(data: data)
+        XCTAssertEqual(json, FlexiJSON(dictionary: ["simple": "json"]))
+    }
+
+    func test_init_data_shouldSetArray() {
+        let data = "[ { \"simple\": \"json\"} ]".dataUsingEncoding(NSUTF8StringEncoding)!
+        let json = FlexiJSON(data: data)
+        XCTAssertEqual(json, FlexiJSON(array: [["simple": "json"]]))
+    }
+
+    func test_init_data_shouldSetFragment() {
+        let data = "\"string\"".dataUsingEncoding(NSUTF8StringEncoding)!
+        let json = FlexiJSON(data: data)
+        XCTAssertEqual(json, FlexiJSON(string: "string"))
+    }
+
+    // MARK: init string
+
+    func test_init_string_shouldSetError_whenInvalidString() {
+        let string = "not json"
+        let json = FlexiJSON(jsonString: string)
+        XCTAssertEqual(json, FlexiJSON(error: "Initialised FlexiJSON with invalid string."))
+    }
+
+    func test_init_string_shouldParseJSON_fromValidString() {
+        let string = "{ \"valid\": \"json\" }"
+        let json = FlexiJSON(jsonString: string)
+        XCTAssertEqual(json, FlexiJSON(dictionary: ["valid": "json"]))
+    }
+
+    func test_init_string_shouldParseJSONFragment() {
+        let string = "\"fragment\""
+        let json = FlexiJSON(jsonString: string)
+        XCTAssertEqual(json, FlexiJSON(string: "fragment"))
+    }
+}
